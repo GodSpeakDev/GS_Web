ed8d67626577c4d3cffd40df5422077828e3827b
Ben Bishop <bbishop@venadotech.com>
2016-12-30 11:05:58 -0600

    Created ApplicationUserInvite model and added it ApplicationUser and ApplicationDbContext

---
+++ GodSpeak/GodSpeak.Web/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GodSpeak.Web.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<GodSpeakModels.ApplicationUserInvite> Invites { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ GodSpeak/GodSpeak.Web/Models/GodSpeakModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GodSpeak.Web.Models
+{
+    public class GodSpeakModels
+    {
+        public class ApplicationUserInvite
+        {
+            public Guid InviteId { get; set; }
+
+            public string Code { get; set; }
+
+            public virtual ApplicationUser User { get; set; }
+        }
+    }
+}
