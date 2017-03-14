4c066ebb52082d44983be9ede07be3234f0ff740
Ben Bishop <bbishop@venadotech.com>
2017-03-14 10:45:57 -0500

    Add LoginRequestObject and make it parameter for Login POST method.

--- GodSpeak/GodSpeak.Web/Controllers/UserController.cs
+++ GodSpeak/GodSpeak.Web/Controllers/UserController.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using GodSpeak.Web.Models;
 
 namespace GodSpeak.Web.Controllers
 {
     public class UserController : ApiController
     {
         [HttpPost]
-        public string Login()
+        public string Login(LoginRequestObject loginRequest)
         {
-            return "hello world";
+            return $"user logged in with {loginRequest.Email} and {loginRequest.Password}";
         }
     }
 
