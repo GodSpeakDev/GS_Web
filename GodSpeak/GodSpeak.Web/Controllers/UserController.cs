3098c4ab599d7e143753154ad702a3a8824ff9b2
Ben Bishop <bbishop@venadotech.com>
2017-03-14 11:26:23 -0500

    Create AuthRepository and register all of its dependencies with Ninject.

--- GodSpeak/GodSpeak.Web/Controllers/UserController.cs
+++ GodSpeak/GodSpeak.Web/Controllers/UserController.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 using GodSpeak.Web.Models;
+using GodSpeak.Web.Repositories;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace GodSpeak.Web.Controllers
 {
     public class UserController : ApiController
     {
-        [HttpPost]
-        public string Login(LoginRequestObject loginRequest)
+        private readonly IAuthRepository _authRepository;
+
+        public UserController(IAuthRepository authRepository)
         {
-            return $"user logged in with {loginRequest.Email} and {loginRequest.Password}";
+            _authRepository = authRepository;
+        }
+
+        [HttpPost]
+        public async Task<string> Login(LoginRequestObject loginRequest)
+        {
+            var user = await _authRepository.FindUser(loginRequest.Email, loginRequest.Password);
+            if (user != null)
+                return $"user logged in with {loginRequest.Email} and {loginRequest.Password}";
+            else
+                return "No luck";
         }
     }
 
