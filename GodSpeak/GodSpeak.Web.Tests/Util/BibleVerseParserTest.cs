6f57d74de847755dd3db56c79d2c5a9b040bc935
Ben Bishop <bbishop@venadotech.com>
2017-03-18 08:50:27 -0500

    Updated bible parser to parse a line into a verse.

---
+++ GodSpeak/GodSpeak.Web.Tests/Util/BibleVerseParserTest.cs
@@ -0,0 +1,22 @@
+﻿using GodSpeak.Web.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+
+namespace GodSpeak.Web.Tests.Util
+{
+    [TestClass]
+    public class BibleVerseParserTest
+    {
+        [TestMethod]
+
+        public void TestParseLine()
+        {
+            var parser = new BibleVerseParser();
+            var verse = parser.ParseLine("Genesis 1:3 Then God said, \"Let there be light\"; and there was light.");
+            verse.Book.ShouldEqual("Genesis");
+            verse.Chapter.ShouldEqual(1);
+            verse.Verse.ShouldEqual(3);
+            verse.Text.ShouldEqual("Then God said, \"Let there be light\"; and there was light.");
+        }
+    }
+}
---
+++ GodSpeak/GodSpeak.Web/Util/BibleVerseParser.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using GodSpeak.Web.Models;
+
+namespace GodSpeak.Web.Util
+{
+    public class BibleVerseParser
+    {
+        public BibleVerse ParseLine(string line)
+        {
+            var parts = line.Split(' ');
+            var locationParts = parts[1].Split(':');
+            return new BibleVerse()
+            {
+                Book = parts[0],
+                Chapter = int.Parse(locationParts[0]),
+                Verse = int.Parse(locationParts[1]),
+                Text = string.Join(" ", parts.ToList().GetRange(2, parts.Length - 2))
+            };
+        }
+
+        
+    }
+}
