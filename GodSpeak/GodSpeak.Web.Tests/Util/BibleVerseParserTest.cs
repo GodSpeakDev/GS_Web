b33178e8f201ed4a9d55b4a18c8f5ed0711328b9
Ben Bishop <bbishop@venadotech.com>
2017-03-18 09:00:06 -0500

    Refactored tests to test multiple verses.

--- GodSpeak/GodSpeak.Web.Tests/Util/BibleVerseParserTest.cs
+++ GodSpeak/GodSpeak.Web.Tests/Util/BibleVerseParserTest.cs
@@ -11,12 +11,24 @@ namespace GodSpeak.Web.Tests.Util
 
         public void TestParseLine()
         {
+            AssertLineParsed("Genesis 1:3 Then God said, \"Let there be light\"; and there was light.", "Genesis", 1, 3, "Then God said, \"Let there be light\"; and there was light.");
+            AssertLineParsed("Exodus 7:1 Then the LORD said to Moses, \"See, I make you {as} God to Pharaoh, and your brother Aaron shall be your prophet.", "Exodus", 7, 1, "Then the LORD said to Moses, \"See, I make you {as} God to Pharaoh, and your brother Aaron shall be your prophet.");
+            AssertLineParsed("Ecclesiastes 10:18 Through indolence the rafters sag, and through slackness the house leaks.", "Ecclesiastes", 10, 18, "Through indolence the rafters sag, and through slackness the house leaks.");
+        }
+
+        private static void AssertLineParsed(string line, string expectedBook, int expectedChapter,
+            int expectedVerse, string expectedText)
+        {
             var parser = new BibleVerseParser();
-            var verse = parser.ParseLine("Genesis 1:3 Then God said, \"Let there be light\"; and there was light.");
-            verse.Book.ShouldEqual("Genesis");
-            verse.Chapter.ShouldEqual(1);
-            verse.Verse.ShouldEqual(3);
-            verse.Text.ShouldEqual("Then God said, \"Let there be light\"; and there was light.");
+            var verse = parser.ParseLine(line);
+
+            verse.Book.ShouldEqual(expectedBook);
+
+            verse.Chapter.ShouldEqual(expectedChapter);
+
+            verse.Verse.ShouldEqual(expectedVerse);
+
+            verse.Text.ShouldEqual(expectedText);
         }
     }
 }
