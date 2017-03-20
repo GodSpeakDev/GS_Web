6a9a17e4d2ace09882284648adcf43f21ce8d672
Ben Bishop <bbishop@venadotech.com>
2017-03-20 14:54:32 -0500

    Add method to traverse and populate a list of invites in a chain.

--- GodSpeak/GodSpeak.Web/Util/UserRegistrationUtil.cs
+++ GodSpeak/GodSpeak.Web/Util/UserRegistrationUtil.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using GodSpeak.Web.Models;
 
 namespace GodSpeak.Web.Util
 {
     public class UserRegistrationUtil
     {
+        public async Task<List<string>> GetParentInviteCodes(string invite, ApplicationDbContext context)
+        {
+            var codes = new List<string>();
+            var profile = await context.Profiles.FirstAsync(p => p.Code == invite);
+
+            while (!string.IsNullOrEmpty(profile.ReferringCode))
+            {
+                var referringCode = profile.ReferringCode;
+                codes.Add(referringCode);
+                profile = await context.Profiles.FirstAsync(p => p.Code == referringCode);
+            }
+
+            return codes;
+        }
+
         public List<MessageCategorySetting> GenerateDefaultMessageCategorySettings(ApplicationDbContext context)
         {
             return
