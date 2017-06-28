13d1833e9921e4c9973415e351d490f2171201f6
Ben Bishop <bbishop@venadotech.com>
2017-06-28 15:14:02 -0500

    Removed unused stuff and wired up manually entering gift requests.

--- GodSpeak/GodSpeak.Web/Controllers/HomeController.cs
+++ GodSpeak/GodSpeak.Web/Controllers/HomeController.cs
@@ -16,7 +16,8 @@ namespace GodSpeak.Web.Controllers
         private readonly IApplicationUserProfileRepository _profileRepository;
         private readonly IInviteRepository _inviteRepository;
 
-        public HomeController(ApplicationUserManager userManager, IApplicationUserProfileRepository profileRepository, IInviteRepository inviteRepository)
+        public HomeController(ApplicationUserManager userManager, IApplicationUserProfileRepository profileRepository,
+            IInviteRepository inviteRepository)
         {
             _userManager = userManager;
             _profileRepository = profileRepository;
@@ -31,35 +32,52 @@ namespace GodSpeak.Web.Controllers
 
             var userId = User.Identity.GetUserId();
             var profile = await _profileRepository.GetByUserId(userId);
+            await UpdateViewBag(profile);
+
+            return View();
+        }
+
+        private async Task UpdateViewBag(ApplicationUserProfile profile)
+        {
             ViewBag.InviteCode = profile.Code;
             ViewBag.UserFirstName = profile.FirstName;
 
-            ViewBag.UnpurchasedAndroidRequests = await _inviteRepository.UnBoughtGifts(profile.Code, PhonePlatforms.Android);
+            ViewBag.UnpurchasedAndroidRequests = await _inviteRepository.UnBoughtGifts(profile.Code,
+                PhonePlatforms.Android);
             ViewBag.PurchasedAndroidRequests = await _inviteRepository.BoughtGifts(profile.Code, PhonePlatforms.Android);
 
             ViewBag.UnpurchasediOSRequests = await _inviteRepository.UnBoughtGifts(profile.Code, PhonePlatforms.iPhone);
             ViewBag.PurchasediOSRequests = await _inviteRepository.BoughtGifts(profile.Code, PhonePlatforms.iPhone);
-
-            return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public async Task<ActionResult> Index(SignUpViewModel model)
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Title = "Your GodSpeak Dashboard";
 
+            var profile = await _profileRepository.GetByCode(model.InviteCode);
+            ViewBag.UserFirstName = profile.FirstName;
+
+            if (!ModelState.IsValid)
+            {
+                await UpdateViewBag(profile);
+                return View(model);
+            }
+
+            if (await _inviteRepository.HasUserAlreadyRequestedGift(model.Email))
+            {
+                ViewBag.ResultMessage = "Sorry, that email has already registered for a gift.";
+                await UpdateViewBag(profile);
+                return View();
+            }
+
+            await _inviteRepository.RegisterGiftRequest(model.Email, model.Platform, model.InviteCode);
+
+            ViewBag.ResultMessage = "Congrats! You have successfully registered a user.";
+            ModelState.Clear();
+            await UpdateViewBag(profile);
             return View();
-        }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult SignUp()
-        {
-            return View();
         }
     }
 }
