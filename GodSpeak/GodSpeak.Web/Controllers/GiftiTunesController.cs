deb3cc426a547fdfe23ee9ffc793e37c018a5662
Ben Bishop <bbishop@venadotech.com>
2017-06-28 16:13:47 -0500

    Setup instruction for gifting itunes desktop.

---
+++ GodSpeak/GodSpeak.Web/Controllers/GiftiTunesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using GodSpeak.Web.Models;
+using GodSpeak.Web.Repositories;
+using Microsoft.AspNet.Identity;
+
+namespace GodSpeak.Web.Controllers
+{
+    public class GiftiTunesController : Controller
+    {
+        private readonly ApplicationUserManager _userManager;
+        private readonly IApplicationUserProfileRepository _profileRepository;
+        private readonly IInviteRepository _inviteRepository;
+
+        public GiftiTunesController(ApplicationUserManager userManager, IApplicationUserProfileRepository profileRepository,
+            IInviteRepository inviteRepository)
+        {
+            _userManager = userManager;
+            _profileRepository = profileRepository;
+            _inviteRepository = inviteRepository;
+        }
+
+        // GET: GiftiTunes
+        public async Task<ActionResult> Index()
+        {
+
+            try
+            {
+                var userId = User.Identity.GetUserId();
+                var profile = await _profileRepository.GetByUserId(userId);
+                var requests = await _inviteRepository.UnBoughtGifts(profile.Code, PhonePlatforms.iPhone);
+                ViewBag.EmailAddresses = String.Join(",", requests.Select(req => req.Email).ToArray());
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+      
+    }
+
+    
+}
